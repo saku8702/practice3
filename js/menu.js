7634538f7cad0ff5675e2c6da051a6e6313ad819
saku8702 <yh19870220@gmail.com>
2021-04-29 22:48:02 +0900

    menu.js変更

--- js/menu.js
+++ js/menu.js
@@ -6,19 +6,15 @@ const body = document.getElementById('js-body');
 
 menu.addEventListener('click', () => {
   nav.classList.toggle('active'), menu.classList.toggle('active');
-
-  if (menu.classList.contains('active')) {
-    body.style.height = '100%';
-    body.style.overflow = 'hidden';
-  } else {
-    body.style.height = 'auto';
-    body.style.overflow = 'scroll';
-  }
+  fixBackground();
 });
 
 nav.addEventListener('click', () => {
   nav.classList.remove('active'), menu.classList.remove('active');
+  fixBackground();
+});
 
+function fixBackground() {
   if (menu.classList.contains('active')) {
     body.style.height = '100%';
     body.style.overflow = 'hidden';
@@ -26,4 +22,4 @@ nav.addEventListener('click', () => {
     body.style.height = 'auto';
     body.style.overflow = 'scroll';
   }
-});
+}
