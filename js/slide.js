dcaa8e60528a3ff4b40c0397444b00fa15581fea
saku8702 <yh19870220@gmail.com>
2021-05-01 23:38:55 +0900

    slide.js修正

--- js/slide.js
+++ js/slide.js
@@ -1,11 +1,12 @@
 'use strict';
 
   var i = 0;
-
-  //↓使用する画像のURL（任意の数）
+  
   var bg = ["img/fv-bgi_01@2x.jpg", "img/fv-bgi_02@2x.jpg", "img/fv-bgi_03@2x.jpg"];
+  
+  function slide_time() {
+  i = (i === bg.length - 1) ? 0 : i + 1;
+  document.getElementById('fv').style.backgroundImage = "url(" + bg[i] + ")";
+  }
 
-  var timer = window.setInterval(function () {
-    i = (i === bg.length - 1) ? 0 : i + 1;
-    document.getElementById('fv').style.backgroundImage = "url(" + bg[i] + ")";
-  }, 3000); //←時間（3000＝3秒）
+  setInterval(slide_time, 3000);
