b422df8e227ae52299d53fe7acf156a8a94e01c9
saku8702 <yh19870220@gmail.com>
2021-05-02 10:34:11 +0900

    header.js 宣言位置修正

--- js/header.js
+++ js/header.js
@@ -1,9 +1,9 @@
 'use strict';
 
-window.addEventListener("scroll", function () {
+const headerElement = document.getElementById("header"); // `#header`セレクタを取得
+const rect = headerElement.getBoundingClientRect(); // 
 
-  const headerElement = document.getElementById("header"); // `#header`セレクタを取得
-  const rect = headerElement.getBoundingClientRect(); // 
+window.addEventListener("scroll", function () {
   const y = rect.top + window.pageYOffset; // Y方向 (縦)にスクロール量を取得（pageYOffset：windowオブジェクト。現在表示位置のY座標を取得）
   if (y > 753) { // 変数yの値が0よりも
     headerElement.classList.remove('hidden'); // 大きければhiddensセレクタを削除する
